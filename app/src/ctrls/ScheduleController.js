8ec6ff57947d061eee6dcabdd68062cbdec5639d
Kevin Chien <kevinchien17@gmail.com>
2017-04-29 03:59:49 -0700

    Sunday Schedule in comments

--- app/src/ctrls/ScheduleController.js
+++ app/src/ctrls/ScheduleController.js
@@ -9,6 +9,21 @@ function ScheduleController($scope) {
     {task: 'Lunch Available', place: 'C-Level Lawn'},
     {tasl: 'Qualification Matches', place: 'Auditorium'},
     {task: 'Group Photos', place: ''}];
+  /* Sunday:
+   8:30 - 9:00 9:00 - 10:30 10:30 - 11:00 11:00 - 12:30 12:30 - 4:00 4:00 - 5:00
+   Check In
+   Lobby (LHS Entrance)
+   Qualification Matches 2
+   Auditorium
+   Alliance Selection
+   Auditorium
+   Lunch Available
+   C-Level Lawn
+   Elimination Matches
+   Auditorium
+   Awards Ceremony
+   Auditorium
+   */
   $scope.range = function(min, max, step) {
     step = step || 1;
     var input = [];
